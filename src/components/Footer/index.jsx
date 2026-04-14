34626ec29e28d5e8da59766cf5da5526beb3e5d0
ZeroOne015645 <lyouxiang89@gmail.com>
2026-04-14 22:15:31 +0800

    Update index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,18 +6,25 @@ import MailOutlineIcon from "@mui/icons-material/MailOutline";
 export default function Footer() {
 	return (
 		<FooterStyled>
-			<div className="made-by">made by Jakub Koper</div>
+			<div className="made-by">由 Jakub Koper 製作 | 由 ZeroOne015645 翻譯</div>
 			<div className="contact">
 				<div className="contact-title">Contact:</div>
 				<div>
-					<div className="contact-sub">
-						<MailOutlineIcon sx={{ fontSize: "2.4rem" }} />
-						<span>jakub.koper@wpc-huh.com</span>
+					<div style={{ display: 'flex', gap: '20px', flexWrap: 'wrap', marginBottom: '10px' }}>
+						<div className="contact-sub">
+							<MailOutlineIcon sx={{ fontSize: "2.4rem" }} />
+							<span>jakub.koper@wpc-huh.com</span>
+						</div>
+						<div className="contact-sub">
+							<MailOutlineIcon sx={{ fontSize: "2.4rem" }} />
+							<span>lyouxiang89@gmail.com</span>
+						</div>
 					</div>
-					<a href="https://github.com/copperhuh">
+
+					<a href="https://github.com/ZeroOne015645/PathDemon-Chinese-Translation">
 						<div className="contact-sub github">
 							<GitHubIcon sx={{ fontSize: "2.4rem" }} />
-							Github
+							Github (Traditional Chinese Version)
 						</div>
 					</a>
 				</div>
